26f4c792decce2b18b724c41c591cb1afd81dbb4
Paolo <paronne@servoy.com>
2020-01-10 17:24:09 +0200

    Update grid services Toolip for order's customer column Workingset JSON

--- cloudSampleSolution/forms/ordersTableView.js
+++ cloudSampleSolution/forms/ordersTableView.js
@@ -1,8 +1,17 @@
 /**
- * @param foundsetindex
- * @param columnindex
+ * @private
+ * @type {String}
+ * @SuppressWarnings (unused)
+ *
+ * @properties={typeid:35,uuid:"08F5A764-2756-4813-8357-5C99E7C761D7"}
+ */
+var tooltipCustomer = 'Show customer at double click';
+
+/**
+ * @param {Number} foundsetindex
+ * @param {Number} columnindex
  * @param {JSRecord<db:/example_data/orders>} record
- * @param event
+ * @param {JSEvent} event
  *
  * @properties={typeid:24,uuid:"B8E6B20B-B389-4204-96D4-36BA81D25C24"}
  * @override
