364acdf8e93433d9b9f789ed7053c6ad739314a0
Paolo <paronne@servoy.com>
2020-11-27 18:37:52 +0200

    Products grid: Remove newRecord method

--- cloudSampleSolution/forms/productsTableView.js
+++
@@ -1,9 +0,0 @@
-/**
- * @properties={typeid:24,uuid:"0170268E-0794-4605-8288-DE3AB2D8463A"}
- * @override
- */
-function newRecord() { 
-	foundset.newRecord();
-	foundset.discontinued = 0;
-	elements.table.editCellAt(foundset.getSelectedIndex(), 0);
-}
