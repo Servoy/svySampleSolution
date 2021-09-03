efe743875ed1def15004626204871744b8767af8
cPecican <85338228+cPecican@users.noreply.github.com>
2021-09-03 12:46:48 +0300

    Update mainNavMenuMobile.js

--- cloudSampleSolution/forms/mainNavMenuMobile.js
+++ cloudSampleSolution/forms/mainNavMenuMobile.js
@@ -1,9 +1,12 @@
 /**
- * @properties={typeid:24,uuid:"135492D0-1A92-46B1-AFBC-995B0C8A37CF"}
+ * @return {Array<CustomType<servoyextra-sidenav.MenuItem>>}
+ * @protected
  * @override
+ * @properties={typeid:24,uuid:"4572AD06-0653-4F4D-9EFC-C80B45C6F0BF"}
  */
 function loadMenuItems() {
 	var menuItems = [];
+	/** @type {CustomType<servoyextra-sidenav.MenuItem>} */
 	var menuItem;
 	var menuSubItems = [];
 	var menuSubItem;
@@ -13,6 +16,7 @@ function loadMenuItems() {
 	menuItem.id = "User";
 	menuItem.text = security.getUserName().toUpperCase();
 	menuItem.iconStyleClass = "fas fa-user";
+	menuItem.styleClass = "text-primary";
 	menuItem.enabled = false;
 	menuItems.push(menuItem);
 	
@@ -24,46 +28,46 @@ function loadMenuItems() {
 	// HOME
 	menuItem = new Object();
 	menuItem.id = "homeDashboard";
-	menuItem.text = "DASHBOARD"
+	menuItem.text = "DASHBOARD";
 	menuItem.iconStyleClass = "fa fa-th-large";
 	menuItems.push(menuItem);
 
 	// CUSTOMERS
 	menuItem = new Object();
 	menuItem.id = "customersTableView";
-	menuItem.text = "CUSTOMERS"
+	menuItem.text = "CUSTOMERS";
 	menuItem.iconStyleClass = "icon-contacts";
 	menuItems.push(menuItem);
 
 	// ORDERS
 	menuItem = new Object();
 	menuItem.id = "ordersTableView";
-	menuItem.text = "ORDERS"
+	menuItem.text = "ORDERS";
 	menuItem.iconStyleClass = "icon-box";
 	menuItems.push(menuItem);
 	
 	// DOCUMENT EDITOR
 	menuItem = new Object();
 	menuItem.id = "documentEditor";
-	menuItem.text = "DOCUMENT EDITOR"
+	menuItem.text = "DOCUMENT EDITOR";
 	menuItem.iconStyleClass = "far fa-file-alt";
 	menuItems.push(menuItem);
 
 	// SECURITY
 	menuItem = new Object();
 	menuItem.id = scopes.svySecurityUX.SVY_SECURITY_UX.TENANT;
-	menuItem.text = "SECURITY"
+	menuItem.text = "SECURITY";
 	menuItem.iconStyleClass = "fas fa-shield-alt";
 
 	menuSubItem = new Object();
 	menuSubItem.id = scopes.svySecurityUX.SVY_SECURITY_UX.TENANT_ROLES;
-	menuSubItem.text = "ROLES"
+	menuSubItem.text = "ROLES";
 	menuSubItem.iconStyleClass = "fa fa-user-shield";
 	menuSubItems.push(menuSubItem);
 
 	menuSubItem = new Object();
 	menuSubItem.id = scopes.svySecurityUX.SVY_SECURITY_UX.TENANT_USERS;
-	menuSubItem.text = "USERS"
+	menuSubItem.text = "USERS";
 	menuSubItem.iconStyleClass = "fa fa-user-shield";
 	menuSubItems.push(menuSubItem);
 
@@ -78,16 +82,16 @@ function loadMenuItems() {
 	// TUTORIAL
 	menuItem = new Object();
 	menuItem.id = "TUTORIAL";
-	menuItem.text = "TUTORIAL"
-	menuItem.styleClass = "font-weight-bold"
+	menuItem.text = "TUTORIAL";
+	menuItem.styleClass = "font-weight-bold";
 	menuItem.iconStyleClass = "fas fa-graduation-cap";
 	menuItems.push(menuItem);
 	
 	// DOWNLOAD IDE
 	menuItem = new Object();
 	menuItem.id = "DOWNLOADIDE";
-	menuItem.text = "DOWNLOAD IDE"
-	menuItem.styleClass = "font-weight-bold"
+	menuItem.text = "DOWNLOAD IDE";
+	menuItem.styleClass = "font-weight-bold";
 	menuItem.iconStyleClass = "icon-cloud-download";
 	menuItems.push(menuItem);
 	
@@ -99,7 +103,7 @@ function loadMenuItems() {
 	// PRIMARY COLORS
 	menuItem = new Object();
 	menuItem.id = "PRIMARYCOLORS";
-	menuItem.text = "PRIMARY COLORS"
+	menuItem.text = "PRIMARY COLORS";
 	menuItem.iconStyleClass = "fas fa-square text-primary border-default border-radius";
 	menuItems.push(menuItem);
 
@@ -111,11 +115,12 @@ function loadMenuItems() {
 }
 
 /**
- * TODO generated, please specify type and doc for the params
+ * @protected 
  * @param menuItemId
  * @param event
+ * @return {Boolean}
  *
- * @properties={typeid:24,uuid:"69A09279-3408-41F9-8D6D-E65BC2541794"}
+ * @properties={typeid:24,uuid:"F13A7870-4303-4BF4-9B72-EB6460907D59"}
  * @override
  */
 function onMenuItemSelected(menuItemId,event) {
