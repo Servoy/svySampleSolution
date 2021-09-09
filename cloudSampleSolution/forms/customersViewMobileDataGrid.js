ed2e9bd7cb7d72e99fb71bec3a6d65c4bc41ae77
cPecican <85338228+cPecican@users.noreply.github.com>
2021-09-09 13:57:56 +0300

    customersViewMobileDataGrid updated

--- cloudSampleSolution/forms/customersViewMobileDataGrid.js
+++ cloudSampleSolution/forms/customersViewMobileDataGrid.js
@@ -3,7 +3,7 @@
  *
  * @private
  *
- * @properties={typeid:24,uuid:"50C13E1F-BFCD-45BC-A8CF-0C27A6BD0AD8"}
+ * @properties={typeid:24,uuid:"449BDF9E-2960-4B6D-BF80-4DD94207DFA7"}
  */
 function onActionSearch(event) {
 	search(searchText,'companyname')
@@ -11,10 +11,26 @@ function onActionSearch(event) {
 
 
 /**
- * @properties={typeid:24,uuid:"F2AA7DAE-3D7D-4526-9936-2A6F304255F3"}
+ * @properties={typeid:24,uuid:"044608F6-03AF-4723-8C16-5FAAC4ABCB2B"}
  * @override
  */
 function newRecord() {
 	_super.newRecord();
 	showForm(forms.customerInfoAdd);
+}
+/**
+ * Called when the mouse is clicked on a row/cell (foundset and column indexes are given).
+ * the foundsetindex is always -1 when there are grouped rows
+ *
+ * @param {number} foundsetindex
+ * @param {number} [columnindex]
+ * @param {JSRecord} [record]
+ * @param {JSEvent} [event]
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"F426FE09-CABF-4948-887F-4BF8288344E6"}
+ */
+function onCellClick(foundsetindex, columnindex, record, event) {
+	showForm(forms.customerView);
 }
